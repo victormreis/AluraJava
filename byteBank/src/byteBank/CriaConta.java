810229c10c6789ebeaf6ca69d0b0c0379c0b8a16
Victor Martins <vitor_jhoul@hotmail.com>
2022-05-30 19:45:46 -0300

    Aula 01 Java OOP

---
+++ byteBank/src/byteBank/CriaConta.java
@@ -0,0 +1,14 @@
+package byteBank;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		
+		Conta pc = new Conta();		
+		pc.saldo = 200;
+		
+		System.out.println(pc.saldo);
+
+	}
+
+}
